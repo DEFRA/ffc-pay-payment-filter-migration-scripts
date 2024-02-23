30a909f04b066326e48ad382be710ceb8bf2f003
John Watson <john.watson1@defra.gov.uk>
2024-02-23 11:43:12 +0000

    Fix rollback

--- ffc-pay-processing/rollback.sql
+++ ffc-pay-processing/rollback.sql
@@ -3,17 +3,27 @@ BEGIN;
 DELETE FROM "outbox"
 USING "completedPaymentRequests"
 WHERE "outbox"."completedPaymentRequestId" = "completedPaymentRequests"."completedPaymentRequestId"
-AND "completedPaymentRequests"."migrated" IS NOT NULL;
+AND "completedPaymentRequests"."migrationId" IS NOT NULL;
 
 DELETE FROM "schedule"
 USING "paymentRequests"
 WHERE "schedule"."paymentRequestId" = "paymentRequests"."paymentRequestId"
-AND "paymentRequests"."migrated" IS NOT NULL;
+AND "paymentRequests"."migrationId" IS NOT NULL;
 
-DELETE FROM "completedInvoiceLines" WHERE "migrated" IS NOT NULL;
-DELETE FROM "completedPaymentRequests" WHERE "migrated" IS NOT NULL;
-DELETE FROM "invoiceLines" WHERE "migrated" IS NOT NULL;
-DELETE FROM "paymentRequests" WHERE "migrated" IS NOT NULL;
-DELETE FROM "holds" WHERE "migrated" IS NOT NULL;
+DELETE FROM "completedInvoiceLines"
+USING "completedPaymentRequests"
+WHERE "completedInvoiceLines"."completedPaymentRequestId" = "completedPaymentRequests"."completedPaymentRequestId"
+AND "completedPaymentRequests"."migrationId" IS NOT NULL;
+
+DELETE FROM "completedPaymentRequests" WHERE "migrationId" IS NOT NULL;
+
+DELETE FROM "invoiceLines"
+USING "paymentRequests"
+WHERE "invoiceLines"."paymentRequestId" = "paymentRequests"."paymentRequestId"
+AND "paymentRequests"."migrationId" IS NOT NULL;
+
+DELETE FROM "paymentRequests" WHERE "migratedId" IS NOT NULL;
+
+DELETE FROM "holds" WHERE "migrationId" IS NOT NULL;
 
 COMMIT;
