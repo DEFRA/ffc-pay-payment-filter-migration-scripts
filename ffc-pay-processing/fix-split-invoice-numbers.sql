5742803a91395d0e7a572a3520cd227cbe57a9e4
John Watson <john.watson1@defra.gov.uk>
2024-02-28 16:13:27 +0000

    Add fix split invoice numbers

--- ffc-pay-processing/fix-split-invoice-numbers.sql
+++ ffc-pay-processing/fix-split-invoice-numbers.sql
@@ -1,41 +1,23 @@
 UPDATE "completedPaymentRequests" 
-SET "invoiceNumber" = 
-    CASE 
-        WHEN 
-            SUBSTRING("invoiceNumber" FROM 9 FOR 1) ~ '[A-Za-z]' 
-            AND 
-            SUBSTRING("invoiceNumber" FROM 10 FOR 1) ~ '[A-Za-z]' 
-        THEN 
-            CONCAT(
-                LEFT("invoiceNumber", 8),
-                RIGHT("invoiceNumber", LENGTH("invoiceNumber") - 9),
-                SUBSTRING("invoiceNumber" FROM 9 FOR 1)
-            )
-        ELSE 
-            "invoiceNumber"
-    END
+SET "invoiceNumber" =
+  CONCAT(
+      LEFT("invoiceNumber", 8),
+      RIGHT("invoiceNumber", LENGTH("invoiceNumber") - 9),
+      SUBSTRING("invoiceNumber" FROM 9 FOR 1)
+  )
 AND "schemeId" = 6
 AND SUBSTRING("invoiceNumber" FROM 9 FOR 1) ~ '[A-Za-z]' 
 AND SUBSTRING("invoiceNumber" FROM 10 FOR 1) ~ '[A-Za-z]';
 
 UPDATE "completedPaymentRequests" 
 SET "invoiceNumber" =
-    CASE 
-        WHEN 
-            SUBSTRING("invoiceNumber" FROM 8 FOR 1) ~ '[A-Za-z]' 
-            AND 
-            SUBSTRING("invoiceNumber" FROM 9 FOR 1) ~ '[A-Za-z]' 
-        THEN 
-            CONCAT(
-                LEFT("invoiceNumber", 1),
-                '0',
-                SUBSTRING("invoiceNumber" FROM 2 FOR 6),
-                SUBSTRING("invoiceNumber" FROM 9),
-                SUBSTRING("invoiceNumber" FROM 8 FOR 1)
-            )
-        ELSE 
-            "invoiceNumber"
-    END
+  CONCAT(
+      LEFT("invoiceNumber", 1),
+      '0',
+      SUBSTRING("invoiceNumber" FROM 2 FOR 6),
+      SUBSTRING("invoiceNumber" FROM 9),
+      SUBSTRING("invoiceNumber" FROM 8 FOR 1)
+  )
 WHERE "schemeId" = 5
 AND SUBSTRING("invoiceNumber" FROM 8 FOR 1) ~ '[A-Za-z]' 
 AND SUBSTRING("invoiceNumber" FROM 9 FOR 1) ~ '[A-Za-z]';
