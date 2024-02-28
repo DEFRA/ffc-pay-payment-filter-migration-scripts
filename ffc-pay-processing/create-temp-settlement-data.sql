92b495f2b4913dab7336467e036c21f1f495615e
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2024-02-28 13:08:46 +0000

    Update create-temp-settlement-data.sql

--- ffc-pay-processing/create-temp-settlement-data.sql
+++ ffc-pay-processing/create-temp-settlement-data.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS public."tempSettlementData"
     "supplierno" bigint,
     "suppliername" character varying(255) COLLATE pg_catalog."default",
     "invoiceNumber" character varying(30) COLLATE pg_catalog."default",
-    value integer
+    value integer,
     "documentDate" timestamp without time zone,
     "invoiceDate" timestamp without time zone,
     "invoiceDueDate" timestamp without time zone,
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS public."tempSettlementData"
     "marketingYear" integer,
     "deliveryBody" character varying(4) COLLATE pg_catalog."default",
     currency character varying(3) COLLATE pg_catalog."default",
-    "invoiceValue" integer
+    "invoiceValue" integer,
     "lastHoldReleaseDate" timestamp without time zone,
     "fesRef" character varying(10) COLLATE pg_catalog."default",
     "paymentMethod" character varying(30) COLLATE pg_catalog."default"
