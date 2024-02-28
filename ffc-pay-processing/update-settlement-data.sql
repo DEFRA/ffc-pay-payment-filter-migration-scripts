d4f7059cedb6f3f54ef8e9a14e4c77683db24c42
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2024-02-28 13:39:06 +0000

    Update update-settlement-data.sql

--- ffc-pay-processing/update-settlement-data.sql
+++ ffc-pay-processing/update-settlement-data.sql
@@ -1,6 +1,6 @@
-UPDATE "completedPaymentRequests" AS c
+UPDATE public."completedPaymentRequests" AS c
     SET "lastSettlement" = t."lastSettlement",
         "settledValue" = t."value" * 100
-    FROM "tempSettlementData" AS t
+    FROM public."tempSettlementData" AS t
     WHERE c."invoiceNumber" = t."invoiceNumber"
         AND (c."lastSettlement" IS NULL OR c."lastSettlement" < t."lastSettlement")
