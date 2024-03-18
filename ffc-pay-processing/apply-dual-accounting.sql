b6d10c5b01dfa09f320d7fec2a39a2ee19cd59b7
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2024-03-18 17:30:29 +0000

    Updated apply-dual-accounting script

--- ffc-pay-processing/apply-dual-accounting.sql
+++ ffc-pay-processing/apply-dual-accounting.sql
@@ -1,6 +1,9 @@
 UPDATE public."completedInvoiceLines" AS l
-  SET "fundCode" = t."fundCode"
-FROM public."tempDualAccounting" AS t
-INNER JOIN public."completedPaymentRequests" AS c
-  ON c."completedPaymentRequestId" = l."completedPaymentRequestId"
-WHERE c."invoiceNumber" = t."invoiceNumber"
+SET "fundCode" = t."fundCode"
+FROM (
+    SELECT l1."completedPaymentRequestId", t."fundCode"
+    FROM public."completedInvoiceLines" AS l1
+    JOIN public."completedPaymentRequests" AS c ON c."completedPaymentRequestId" = l1."completedPaymentRequestId"
+    JOIN public."tempDualAccounting" AS t ON c."invoiceNumber" = t."invoiceNumber"
+) AS t
+WHERE l."completedPaymentRequestId" = t."completedPaymentRequestId";
