c4571abed3519c75445ef4076fb0c608b0a2a056
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2024-10-18 10:11:09 +0100

    split out CS and BPS completed invoice lines

--- ffc-pay-processing/load-completed-invoice-lines.sql
+++ ffc-pay-processing/load-completed-invoice-lines.sql
@@ -1,3 +1,5 @@
+// IMPORT CS invoice lines
+
 INSERT INTO "completedInvoiceLines"(
   "completedPaymentRequestId",
   "accountCode",
@@ -28,4 +30,40 @@ INNER JOIN "completedPaymentRequests"
 LEFT JOIN "completedInvoiceLines"
   ON "completedPaymentRequests"."completedPaymentRequestId" = "completedInvoiceLines"."completedPaymentRequestId"
 WHERE "completedInvoiceLines"."completedPaymentRequestId" IS NULL
+  AND "completedInvoiceLines"."schemeId" = 5
+  AND "tempCompletedInvoiceLines"."description" NOT LIKE 'N00%';
+
+// IMPORT BPS invoice lines
+
+INSERT INTO "completedInvoiceLines"(
+  "completedPaymentRequestId",
+  "accountCode",
+  "fundCode",
+  description,
+  value,
+  "schemeCode",
+  convergence,
+  "deliveryBody",
+  "agreementNumber",
+  "marketingYear",
+  "stateAid")
+SELECT
+  "completedPaymentRequests"."completedPaymentRequestId",
+  "tempCompletedInvoiceLines"."accountCode",
+  "tempCompletedInvoiceLines"."fundCode",
+  "tempCompletedInvoiceLines".description,
+  "tempCompletedInvoiceLines".value,
+  "tempCompletedInvoiceLines"."schemeCode",
+  "tempCompletedInvoiceLines".convergence,
+  "tempCompletedInvoiceLines"."deliveryBody",
+  "tempCompletedInvoiceLines"."agreementNumber",
+  "tempCompletedInvoiceLines"."marketingYear",
+  "tempCompletedInvoiceLines"."stateAid"
+FROM "tempCompletedInvoiceLines"
+INNER JOIN "completedPaymentRequests"
+  ON "tempCompletedInvoiceLines"."migrationId" = "completedPaymentRequests"."migrationId"
+LEFT JOIN "completedInvoiceLines"
+  ON "completedPaymentRequests"."completedPaymentRequestId" = "completedInvoiceLines"."completedPaymentRequestId"
+WHERE "completedInvoiceLines"."completedPaymentRequestId" IS NULL
+  AND "completedInvoiceLines"."schemeId" = 6
   AND "tempCompletedInvoiceLines"."description" NOT LIKE 'N00%';
