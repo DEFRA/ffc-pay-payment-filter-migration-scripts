66732a457c3e2e55ab420ed19dbc688ca97e9c1b
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2024-10-21 16:09:54 +0100

    Update rollback.sql

--- ffc-pay-request-editor/rollback.sql
+++ ffc-pay-request-editor/rollback.sql
@@ -2,9 +2,19 @@ DELETE FROM "invoiceLines"
 USING "paymentRequests"
 WHERE "invoiceLines"."paymentRequestId" = "paymentRequests"."paymentRequestId"
   AND "paymentRequests"."schemeId" IN (5, 6, 7);
+ 
+DELETE FROM "qualityChecks"
+USING "paymentRequests"
+WHERE "qualityChecks"."paymentRequestId" = "paymentRequests"."paymentRequestId"
+  AND "paymentRequests"."schemeId" IN (5, 6, 7);
+
+DELETE FROM "manualLedgerPaymentRequest"
+USING "paymentRequests"
+WHERE "manualLedgerPaymentRequest"."paymentRequestId" = "paymentRequests"."paymentRequestId"
+  AND "paymentRequests"."schemeId" IN (5, 6, 7);
 
 DELETE FROM "paymentRequests"
 WHERE "schemeId" IN (5, 6, 7);
-
+ 
 DELETE FROM "debtData"
 WHERE "migrated" IS NOT NULL;
