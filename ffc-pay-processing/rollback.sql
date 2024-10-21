029980f6cff5a004a890cbb2bb15bdf1700c8f10
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2024-10-21 16:25:41 +0100

    Update rollback.sql

--- ffc-pay-processing/rollback.sql
+++ ffc-pay-processing/rollback.sql
@@ -13,13 +13,33 @@ USING "completedPaymentRequests"
 WHERE "completedInvoiceLines"."completedPaymentRequestId" = "completedPaymentRequests"."completedPaymentRequestId"
 AND "completedPaymentRequests"."migrationId" IS NOT NULL;
 
-DELETE FROM "completedPaymentRequests" WHERE "migrationId" IS NOT NULL;
+DELETE FROM "completedPaymentRequests"
+WHERE "migrationId" IS NOT NULL
+AND "schemeId" = 5;
+
+DELETE FROM "completedPaymentRequests"
+WHERE "migrationId" IS NOT NULL
+AND "schemeId" = 6;
+
+DELETE FROM "completedPaymentRequests"
+WHERE "migrationId" IS NOT NULL
+AND "schemeId" = 7;
 
 DELETE FROM "invoiceLines"
 USING "paymentRequests"
 WHERE "invoiceLines"."paymentRequestId" = "paymentRequests"."paymentRequestId"
 AND "paymentRequests"."migrationId" IS NOT NULL;
 
-DELETE FROM "paymentRequests" WHERE "migrationId" IS NOT NULL;
+DELETE FROM "paymentRequests"
+WHERE "migrationId" IS NOT NULL
+AND "schemeId" = 5;
+
+DELETE FROM "paymentRequests"
+WHERE "migrationId" IS NOT NULL
+AND "schemeId" = 6;
+
+DELETE FROM "paymentRequests"
+WHERE "migrationId" IS NOT NULL
+AND "schemeId" = 7;
 
 DELETE FROM "holds" WHERE "migrationId" IS NOT NULL;
